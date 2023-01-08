2341e19ae4b1387bfa3e6e668c910000729661ac
bang-star <bang77060986@gmail.com>
2023-01-08 21:37:58 +0900

    #14 - Kakao 주소검색 API 구현

---
+++ src/main/java/com/example/api/dto/DocumentDto.java
@@ -0,0 +1,21 @@
+package com.example.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentDto {
+
+    @JsonProperty("address_name")
+    private String addressName;     // 전체 지번 주소 또는 전체 도로명 주소, 입력에 따라 결정됨
+
+    @JsonProperty("y")
+    private double latitude;        // 위도(latitude)
+
+    @JsonProperty("x")
+    private double longitude;       // 경도(longitude)
+}
---
+++ src/main/java/com/example/api/dto/MetaDto.java
@@ -0,0 +1,15 @@
+package com.example.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDto {
+
+    @JsonProperty("total_count")
+    private Integer totalCount;     // 검색어에 검색된 문서 수
+}
