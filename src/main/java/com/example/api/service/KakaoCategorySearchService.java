82f28d7befa8785a4168e493e151bc9d66c868ac
bang-star <bang77060986@gmail.com>
2023-01-20 16:47:14 +0900

    #41 - 카카오 키워드 장소 검색 API 적용하기
    공공기관에서 제공받은 약국 정보는 제한적인 부분이 있기 때문에 카카오 키워드 장소 검색 API를 통해 해결하였다.

---
+++ src/main/java/com/example/api/service/KakaoCategorySearchService.java
@@ -0,0 +1,54 @@
+package com.example.api.service;
+
+import com.example.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoCategorySearchService {
+
+    private final KakaoUriBuilderService kakaoUriBuilderService;
+
+    private final RestTemplate restTemplate;
+
+    private static final String PHARMACY_CATEGORY = "PM9"; // 약국 카테고리
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+    @Retryable(
+            value = {RuntimeException.class},
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 2000)
+    )
+    public KakaoApiResponseDto requestPharmacyCategorySearch(double latitude, double longitude, double radius) {
+
+        URI uri = kakaoUriBuilderService.buildUriByCategorySearch(latitude, longitude, radius, PHARMACY_CATEGORY);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK "+ kakaoRestApiKey);
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+
+    @Recover
+    public KakaoApiResponseDto recover(RuntimeException e, double latitude, double longitude, double radius) {
+        log.error("All the retries failed. latitude, longitude: {}, {} radius: {}, error: {}", latitude, longitude, radius, e.getMessage());
+
+        return null;
+    }
+}
